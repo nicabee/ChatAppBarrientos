d7f5dadd8f87a3f1ccf08837df84bc1ab6a4415c
Monica <monicabee358@gmail.com>
2020-04-29 18:48:57 +0800

    changed Profile Page title

--- ChatApp-Barrientos/ChatApp-Barrientos.Android/obj/Debug/90/android/environment.x86.s
+++ ChatApp-Barrientos/ChatApp-Barrientos.Android/obj/Debug/90/android/environment.x86.s
@@ -61,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"acc5c6a8-f1c5-4d16-bc68-6d3dc102655e"
+	.asciz	"66f67a5e-42d5-4006-b951-640377864b6b"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
